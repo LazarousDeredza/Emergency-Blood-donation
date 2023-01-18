8e7d5771026c6b649a65b34d6cf20b2e503039e1
LazarousDeredza <lazarousderedza99@gmail.com>
2023-01-18 14:51:55 -0800

    l

---
+++ app/src/main/java/com/example/emergencyblooddonation/AddHospital.java
@@ -0,0 +1,14 @@
+package com.example.emergencyblooddonation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddHospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_hospital);
+    }
+}
